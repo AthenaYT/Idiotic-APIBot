498afe20453e64b15e89d09f2a7d20e0945ac635
Jack N <jacknagle22@gmail.com>
2018-02-13 00:55:02 +1100

    Send decline message to offile

--- eventsRaw/messageReactionAdd.js
+++ eventsRaw/messageReactionAdd.js
@@ -6,25 +6,30 @@ module.exports = class {
 
   async run(data) {
     const reaction = data.d;
+    const supportGuild = this.client.guilds.get("406609139184304139");
+    const officeChannel = supportGuild.channels.get("406609139184304139");
     const user = this.client.users.get(reaction.user_id) || await this.client.fetchUser(reaction.user_id).catch(() => null);
     if (!user) return;
     if (user.id === this.client.user.id) return;
     const channel = this.client.channels.get(reaction.channel_id);
     if (!channel || channel.type !== "text" || channel.permissionsFor(this.client.user).has("VIEW_CHANNEL") === false) return false;
     const message = await channel.fetchMessage(reaction.message_id);
+    const applicationUser = this.client.users.get(message.embeds[0].fields[0].embed.footer.text);
     switch (reaction.emoji.name) {
       case "approved": {
         console.log("approved");
-        const userChannel = this.client.users.get(message.embeds[0].fields[0].embed.footer.text);
         await message.clearReactions();
-        await this.client.generate(userChannel, `Approved by ${user.tag}`);
+        await this.client.generate(applicationUser, `Approved by ${user.tag}`);
         break;
       }
-      case "declined":
+      case "declined": {
         console.log("declined");
         await message.clearReactions();
-        await this.client.awaitDmReply(user, "Why have you declined this key?");
+        const declinedMsg = await this.client.awaitDmReply(user, "Why have you declined this key?");
+        if (!declinedMsg) return; // TODO: handle this somehow better?
+        officeChannel.send(`${user} declined ${applicationUser.tag} with reason: ${declinedMsg}`);
         break;
+      }
       default: return;
     }
   }
