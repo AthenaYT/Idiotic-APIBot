f27fbd429f9c8fd4f20b60e639a6829966dfe7ae
Jack N <jacknagle22@gmail.com>
2018-02-13 00:18:29 +1100

    Fix a fail

--- structures/ApiClient.js
+++ structures/ApiClient.js
@@ -14,7 +14,7 @@ class APIBot extends Client {
     this.aliases = new Collection();
     this.logger = require("../util/Logger");
     this.database = Database.db;
-
+    this.levelCache = {};
     this.ready = false;
 
     this.once("ready", () => this.ready());
@@ -34,7 +34,7 @@ class APIBot extends Client {
   permlevel(message) {
     let permlvl = 0;
   
-    const permOrder = this.client.config.permLevels.slice(0).sort((p, c) => p.level < c.level ? 1 : -1);
+    const permOrder = this.config.permLevels.slice(0).sort((p, c) => p.level < c.level ? 1 : -1);
   
     while (permOrder.length) {
       const currentLevel = permOrder.shift();
@@ -49,14 +49,14 @@ class APIBot extends Client {
   
   loadCommand(commandPath, commandName) {
     try {
-      const props = new (require(`${commandPath}${path.sep}${commandName}`))(this.client);
-      this.client.logger.log(`Loading Command: ${props.help.name}. 👌`, "log");
+      const props = new (require(`${commandPath}${path.sep}${commandName}`))(this);
+      this.logger.log(`Loading Command: ${props.help.name}. 👌`, "log");
       props.conf.location = commandPath;
-      if (props.init) props.init(this.client);
+      if (props.init) props.init(this);
       
-      this.client.commands.set(props.help.name, props);
+      this.commands.set(props.help.name, props);
       props.conf.aliases.forEach(alias => {
-        this.client.aliases.set(alias, props.help.name);
+        this.aliases.set(alias, props.help.name);
       });
       return false;
     } catch (e) {
@@ -66,15 +66,15 @@ class APIBot extends Client {
   
   async unloadCommand(commandPath, commandName) {
     let command;
-    if (this.client.commands.has(commandName)) {
-      command = this.client.commands.get(commandName);
-    } else if (this.client.aliases.has(commandName)) {
-      command = this.client.commands.get(this.client.aliases.get(commandName));
+    if (this.commands.has(commandName)) {
+      command = this.commands.get(commandName);
+    } else if (this.aliases.has(commandName)) {
+      command = this.commands.get(this.aliases.get(commandName));
     }
     if (!command) return `The command \`${commandName}\` doesn"t seem to exist, nor is it an alias. Try again!`;
   
     if (command.shutdown) {
-      await command.shutdown(this.client);
+      await command.shutdown(this);
     }
     delete require.cache[require.resolve(`${commandPath}${path.sep}${commandName}.js`)];
     return false;
@@ -84,23 +84,22 @@ class APIBot extends Client {
     klaw("../commands").on("data", (item) => {
       const cmdFile = path.parse(item.path);
       if (!cmdFile.ext || cmdFile.ext !== ".js") return;
-      const response = this.client.loadCommand(cmdFile.dir, `${cmdFile.name}${cmdFile.ext}`);
-      if (response) this.client.logger.error(response);
+      const response = this.loadCommand(cmdFile.dir, `${cmdFile.name}${cmdFile.ext}`);
+      if (response) this.logger.error(response);
     });
     
     const evtFiles = await readdir("../events/");
-    this.client.logger.log(`Loading a total of ${evtFiles.length} events.`, "log");
+    this.logger.log(`Loading a total of ${evtFiles.length} events.`, "log");
     evtFiles.forEach(file => {
       const eventName = file.split(".")[0];
-      const event = new (require(`../events/${file}`))(this.client);
+      const event = new (require(`../events/${file}`))(this);
       this.client.on(eventName, (...args) => event.run(...args));
       delete require.cache[require.resolve(`../events/${file}`)];
     });
     
-    this.client.levelCache = {};
-    for (let i = 0; i < this.client.config.permLevels.length; i++) {
-      const thisLevel = this.client.config.permLevels[i];
-      this.client.levelCache[thisLevel.name] = thisLevel.level;
+    for (let i = 0; i < this.config.permLevels.length; i++) {
+      const thisLevel = this.config.permLevels[i];
+      this.levelCache[thisLevel.name] = thisLevel.level;
     }
   }
   
