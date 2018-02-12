8c1201bf734ec6112850e64e06b8d5b819cbaae8
Jack N <jacknagle22@gmail.com>
2018-02-12 14:53:07 +1100

    Do we even need this?

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-if (process.version.slice(1).split(".")[0] < 8) throw new Error("Node 8.0.0 or higher is required. Update Node on your system.");
 const ApiClient = require("./structures/ApiClient");
-
 const client = new ApiClient();
 
 client.login(client.token);
